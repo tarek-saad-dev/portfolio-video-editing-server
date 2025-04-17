3401d36514604404a1c9792002c758843737367d
Tarek <saadfouad1976t@gmail.com>
2025-04-17 11:49:02 +0200

    .....

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // preflight support
+// app.options('*', cors(corsOptions)); // preflight support
 
 
 
